606127aaa8b1ba7692471b73c90b2a09a625d13e
Andrea Machado <andreamgaropaba@gmail.com>
2020-07-17 16:07:27 -0300

    Inclusão do filter para fazer a validação de acesso para a aplicação.

---
+++ src/main/java/com/andrea/restfull/filter/FilterJava.java
@@ -0,0 +1,36 @@
+package com.andrea.restfull.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FilterJava implements Filter {
+
+
+    private Logger logger = LoggerFactory.getLogger(FilterJava.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        logger.info("Chegamos no portão!!");
+        HttpServletRequest req = (HttpServletRequest) request;
+        Enumeration<String> headerNames = req.getHeaderNames();
+        Map<String, String> mapHeaders = Collections.list(headerNames)
+                .stream().collect(Collectors.toMap(it -> it, req::getHeader));
+        if (mapHeaders.get("authorization") != null && mapHeaders.get("authorization").equals("BATATINHA")){
+            chain.doFilter(request,response);
+        } else {
+            HttpServletResponse res = (HttpServletResponse) response;
+            res.sendError(403);
+            //return;
+        }
+        logger.info("Saiu agora do portão do castelo.");
+    }
+}
